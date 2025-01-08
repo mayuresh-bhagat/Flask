ca17a06dbd85e72cf190edc4e29091f0d870111a
mayuresh-bhagat <mayureshbhagat358@gmail.com>
2025-01-08 17:33:06 +0530

    Login Authentication

--- dbapplication/models.py
+++ dbapplication/models.py
@@ -1,4 +1,5 @@
 from app import db
+from flask_login import UserMixin
 
 class Person(db.Model):
     __tablename__ = 'persons'
@@ -10,3 +11,19 @@ class Person(db.Model):
 
     def __repr__(self):
         return f"Person(pid={self.pid}, name={self.name}, age={self.age}, job={self.job})"
+    
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    uid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String)
+    description = db.Column(db.String)
+    
+    def __repr__(self):
+        return f"<User:{self.username}, role:{self.role}>"
+    
+    def get_id(self):
+        return self.uid
--- dbapplication/routes.py
+++ dbapplication/routes.py
@@ -1,10 +1,73 @@
-from flask import render_template, request
-from models import Person
+from flask import render_template, request, redirect, url_for
+from models import Person, User
+from flask_login import login_user, logout_user, current_user, login_required
 
-def register_routes(app, db):
+def register_routes(app, db, bcrypt):
 
-    @app.route('/', methods=['GET', 'POST'])
+    @app.route('/')
     def index():
+        return render_template('index.html')
+    
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+        if request.method == 'GET':
+            return render_template('signup.html')
+        elif request.method == 'POST':
+            if 'username' in request.form.keys() and 'password' in request.form.keys():
+                username = request.form.get('username')
+                password = request.form.get('password')
+
+                hashed_password = bcrypt.generate_password_hash(password)
+
+                user = User(username=username, password=hashed_password)
+
+                db.session.add(user)
+                db.session.commit()
+                # session['message'] == 'User Created Successfully'
+                return redirect(url_for('index'))
+            else :
+                return redirect(url_for('signup'))
+            
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'GET':
+            return render_template('login.html')
+        elif request.method == 'POST':
+            if 'username' in request.form.keys() and 'password' in request.form.keys():
+                username = request.form.get('username')
+                password = request.form.get('password')
+                
+                user = User.query.filter(User.username == username).first()
+                if user == None:
+                    return render_template('login.html', message='User not found')
+                else :
+                    if bcrypt.check_password_hash(user.password, password):
+                        login_user(user)
+                        return redirect(url_for('index'))
+                    else:
+                        return 'Failed'
+            
+            else :
+                return redirect(url_for('login'))
+    
+    
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        return redirect(url_for('index'))
+    
+    @app.route('/secret')
+    @login_required
+    def secret():
+        return 'Secret Page'
+
+
+
+    
+    # for DB Application
+    @app.route('/create_user', methods=['GET', 'POST'])
+    def create_user():
         if request.method == 'GET':
             persons = Person.query.all()
             return render_template('index.html', Persons=persons)
@@ -37,4 +100,4 @@ def register_routes(app, db):
         person = Person.query.filter(Person.pid == pid).first()
 
         return render_template('details.html', person=person)
-            
+    
